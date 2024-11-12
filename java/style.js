bfded9be175f95b7cfd16b8e3e89512cc26a0aff
frjlnsyh <frjlnsyh@gmail.com>
2024-11-12 18:25:13 +0700

    Pembaharuan CV dari tampilan handphone

--- java/style.js
+++ java/style.js
@@ -1,16 +1,16 @@
-document.querySelector(".show").addEventListener("click", function() {
+document.querySelector(".show").addEventListener("click", function () {
   const navUl = document.getElementById("nav-list");
   const hide = document.querySelector(".hide");
-  
+
   this.classList.add("active");
   navUl.classList.add("active");
   hide.classList.add("active");
 });
 
-document.querySelector(".hide").addEventListener("click", function() {
+document.querySelector(".hide").addEventListener("click", function () {
   const navUl = document.getElementById("nav-list");
   const show = document.querySelector(".show");
-  
+
   this.classList.remove("active");
   navUl.classList.remove("active");
   show.classList.remove("active");
@@ -23,14 +23,14 @@ const hide = document.querySelector(".hide");
 const navUl = document.getElementById("nav-list");
 const show = document.querySelector(".show");
 
-  links.forEach(link => {
-    link.addEventListener('click', function() {
-      links.forEach(item => item.classList.remove('active'));
+links.forEach(link => {
+  link.addEventListener('click', function () {
+    links.forEach(item => item.classList.remove('active'));
 
-      this.classList.add('active');
-      // menambahkan active setiap click salah satu navigasi
-      hide.classList.add("active");
-      navUl.classList.add("active");
-      show.classList.add("active");
-    });
+    this.classList.add('active');
+    // menambahkan active setiap click salah satu navigasi
+    hide.classList.add("active");
+    navUl.classList.add("active");
+    show.classList.add("active");
   });
+});
